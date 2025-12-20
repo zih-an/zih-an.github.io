4207a0c2b1e50a6fae4959ff8ef1964beb29cbb9
zih-an <zihanzhao777@gmail.com>
2025-12-21 03:58:03 +0800

    nextjs refactoring

--- _next/static/3-8oOlmMD7EHk9Qg5t2gT/_buildManifest.js
+++ _next/static/3-8oOlmMD7EHk9Qg5t2gT/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/zih-an.github.io//_next/:path+",
+        "destination": "/zih-an.github.io/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
